11d4211daccce25ea2ab30d206e12d087c273c71
Yurii Leniv <supusman32@gmail.com>
2026-03-29 14:51:36 +0300

    iljiuhygdfasydfs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,8 +8,8 @@ area.addEventListener('click', (e) => {
   const clickY = e.clientY - ob.top - area.clientTop - spider.clientHeight / 2;
   const clickX = e.clientX - ob.left - area.clientLeft - spider.clientWidth / 2;
 
-  const maxY = ob.bottom - ob.top - area.clientTop * 2 - spider.clientHeight;
-  const maxX = ob.right - ob.left - area.clientLeft * 2 - spider.clientWidth;
+  const maxY = area.clientHeight - spider.clientHeight;
+  const maxX = area.clientWidth - spider.clientWidth;
 
   const top1 = Math.max(0, Math.min(clickY, maxY));
   const left1 = Math.max(0, Math.min(clickX, maxX));
